28a60f90133beb1a827d42d104bea399e0e7bb93
winzer <aitajprince100@gmail.com>
2025-03-07 10:43:57 +0300

    Added user-routes.js to routes

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userController = require('../middleware/user-controller.js');
+const router = express.Router();
+
+router.post('/', userController.createUser);
+router.get('/:id', userController.getUser);
+router.get('/', userController.getUsers);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
